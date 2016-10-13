96fb5d8fa1da237f408e3e3724744759fed06e2a
Kenedy Lukito <kenedylukito@yahoo.com>
2016-10-13 22:31:05 +0700

    Notepad++ - like

---
+++ Latihan_3_1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿using System.Windows.Forms;
+namespace Latihan_3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.fsize = new System.Windows.Forms.ToolStripComboBox();
+            this.boldButton = new System.Windows.Forms.ToolStripButton();
+            this.italicButton = new System.Windows.Forms.ToolStripButton();
+            this.underlineButton = new System.Windows.Forms.ToolStripButton();
+            this.ffamily = new System.Windows.Forms.ToolStripComboBox();
+            this.fcolor = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(5, 33);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(570, 278);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.SelectionChanged += new System.EventHandler(this.richTextBox1_SelectionChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fsize,
+            this.boldButton,
+            this.italicButton,
+            this.underlineButton,
+            this.ffamily,
+            this.fcolor});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(581, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // fsize
+            // 
+            this.fsize.Name = "fsize";
+            this.fsize.Size = new System.Drawing.Size(121, 25);
+            this.fsize.SelectedIndexChanged += new System.EventHandler(this.fsize_SelectedIndexChanged);
+            this.fsize.KeyUp += new System.Windows.Forms.KeyEventHandler(this.fsize_typing);
+            // 
+            // boldButton
+            // 
+            this.boldButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.boldButton.Image = ((System.Drawing.Image)(resources.GetObject("boldButton.Image")));
+            this.boldButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.boldButton.Name = "boldButton";
+            this.boldButton.Size = new System.Drawing.Size(23, 22);
+            this.boldButton.Text = "toolStripButton1";
+            this.boldButton.Click += new System.EventHandler(this.boldButton_Click_1);
+            // 
+            // italicButton
+            // 
+            this.italicButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.italicButton.Image = ((System.Drawing.Image)(resources.GetObject("italicButton.Image")));
+            this.italicButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.italicButton.Name = "italicButton";
+            this.italicButton.Size = new System.Drawing.Size(23, 22);
+            this.italicButton.Text = "toolStripButton2";
+            this.italicButton.Click += new System.EventHandler(this.italicButton_Click);
+            // 
+            // underlineButton
+            // 
+            this.underlineButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.underlineButton.Image = ((System.Drawing.Image)(resources.GetObject("underlineButton.Image")));
+            this.underlineButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.underlineButton.Name = "underlineButton";
+            this.underlineButton.Size = new System.Drawing.Size(23, 22);
+            this.underlineButton.Text = "toolStripButton3";
+            this.underlineButton.Click += new System.EventHandler(this.underlineButton_Click);
+            // 
+            // ffamily
+            // 
+            this.ffamily.Name = "ffamily";
+            this.ffamily.Size = new System.Drawing.Size(200, 25);
+            this.ffamily.SelectedIndexChanged += new System.EventHandler(this.ffamily_SelectedIndexChanged);
+            this.ffamily.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ffamily_typing);
+            // 
+            // fcolor
+            // 
+            this.fcolor.Name = "fcolor";
+            this.fcolor.Size = new System.Drawing.Size(170, 25);
+            this.fcolor.SelectedIndexChanged += new System.EventHandler(this.fcolor_SelectedIndexChanged);
+            this.fcolor.KeyUp += new System.Windows.Forms.KeyEventHandler(this.fcolor_typing);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 330);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox fsize;
+        private System.Windows.Forms.ToolStripButton boldButton;
+        private System.Windows.Forms.ToolStripButton italicButton;
+        private System.Windows.Forms.ToolStripButton underlineButton;
+        private System.Windows.Forms.ToolStripComboBox ffamily;
+        private System.Windows.Forms.ToolStripComboBox fcolor;
+    }
+}
+
---
+++ Latihan_3_1/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Collections;
+
+namespace Latihan_3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Color color = new Color();
+            PropertyInfo[] colour = color.GetType().GetProperties();
+
+            ArrayList ColorList = new ArrayList();
+            Type colorType = typeof(System.Drawing.Color);
+            PropertyInfo[] propInfoList = colorType.GetProperties(BindingFlags.Static |
+                                          BindingFlags.DeclaredOnly | BindingFlags.Public);
+            foreach (PropertyInfo c in propInfoList)
+            {
+                this.fcolor.Items.Add(c.Name);
+            }
+
+            List<string> fonts = new List<string>();
+
+            foreach (FontFamily font in System.Drawing.FontFamily.Families)
+            {
+                fonts.Add(font.Name);
+            }
+
+            foreach(string k in fonts)
+            {
+                ffamily.Items.Add(k);
+            }
+            for (int i=8;i<=72;i++)
+            {
+                fsize.Items.Add(i);
+            }
+
+            this.fcolor.ComboBox.DrawMode = DrawMode.OwnerDrawFixed;
+            foreach (PropertyInfo c in colour)
+            {
+                if (c.PropertyType == typeof(System.Drawing.Color))
+                {
+                    fcolor.Items.Add(c.Name);
+                }
+            }
+            this.fcolor.ComboBox.DrawItem += new DrawItemEventHandler(fcolor_DrawItem);
+
+            fsize.Text = "11";
+            ffamily.Text = "Times New Roman";
+            fcolor.Text = "Black";
+            boldButton.CheckOnClick = true;
+            italicButton.CheckOnClick = true;
+            underlineButton.CheckOnClick = true;
+        }
+
+        private void fcolor_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Rectangle rect = e.Bounds;
+            if (e.Index >= 0)
+            {
+                string n = ((ComboBox)sender).Items[e.Index].ToString();
+                Font f = new Font("Calibri", 9, FontStyle.Regular);
+                Color c = Color.FromName(n);
+                Brush b = new SolidBrush(c);
+                g.DrawString(n, f, Brushes.Black, rect.X, rect.Top);
+                g.FillRectangle(b, rect.X + 110, rect.Y + 5,
+                                rect.Width - 10, rect.Height - 10);
+            }
+
+        }
+        private void fsize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ToolStripComboBox fokus = (ToolStripComboBox)sender;
+            if (!fokus.Focused) return;
+            styling();
+        }
+
+        private void fsize_typing(object sender, EventArgs e)
+        {
+            styling();
+        }
+        private void ffamily_typing(object sender, EventArgs e)
+        {
+            styling();
+        }
+        private void fcolor_typing(object sender, EventArgs e)
+        {
+            ubahwarna();
+        }
+        private void ffamily_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            styling();
+        }
+        private void fcolor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ubahwarna();
+        }
+        private void boldButton_Click_1(object sender, EventArgs e)
+        {
+            styling();
+        }
+
+        private void italicButton_Click(object sender, EventArgs e)
+        {
+            styling();
+        }
+
+        private void underlineButton_Click(object sender, EventArgs e)
+        {
+            styling();
+        }
+        private void richTextBox1_SelectionChanged(object sender, EventArgs e)
+        {
+            boldButton.Checked = false;
+            italicButton.Checked = false;
+            underlineButton.Checked = false;
+            if(richTextBox1.SelectionFont!=null)
+            {
+                if (richTextBox1.SelectionFont.Style.ToString().IndexOf("Bold")>=0) boldButton.Checked = true;
+                if (richTextBox1.SelectionFont.Style.ToString().IndexOf("Italic") >= 0) italicButton.Checked = true;
+                if (richTextBox1.SelectionFont.Style.ToString().IndexOf("Underline") >= 0) underlineButton.Checked = true;
+                fsize.Text = richTextBox1.SelectionFont.Size.ToString();
+                ffamily.Text = richTextBox1.SelectionFont.FontFamily.Name;
+                fcolor.Text = richTextBox1.SelectionColor.Name;
+            }
+            else
+            {
+                ffamily.Text = "";
+                fsize.Text = "";
+                fcolor.Text = "";
+            }
+
+        }        
+       private void ubahwarna()
+        {
+            richTextBox1.SelectionColor = Color.FromName(fcolor.Text);
+            //richTextBox1.Focus();
+        }
+        private void styling()
+        {
+            System.Drawing.FontStyle state = (boldButton.Checked)? FontStyle.Bold : FontStyle.Regular;
+            state|=(italicButton.Checked)? FontStyle.Italic : FontStyle.Regular;
+            state|=(underlineButton.Checked)? FontStyle.Underline : FontStyle.Regular;
+            try
+            {
+                Font font = new Font((ffamily.Text == "") ? "Times New Roman" : ffamily.Text, (fsize.Text == "") ? 11 : Convert.ToSingle(fsize.Text), state);
+                richTextBox1.SelectionFont = font;
+            }
+            catch
+            {
+                return;
+            }
+        }     
+    }
+}
